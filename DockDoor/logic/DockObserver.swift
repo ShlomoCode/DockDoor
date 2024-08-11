ea9af9ada3910875d1371d4fad9b1df798536730
Shlomo <78599753+ShlomoCode@users.noreply.github.com>
2024-08-12 00:02:25 +0300

    Update DockObserver.swift

--- DockDoor/logic/DockObserver.swift
+++ DockDoor/logic/DockObserver.swift
@@ -12,7 +12,7 @@ func handleSelectedDockItemChangedNotification(observer _: AXObserver, element _
 }
 
 final class DockObserver {
-    static let shared = DockObserver()
+    static let shared =         DockObserver()
 
     var axObserver: AXObserver?
     var lastAppUnderMouse: NSRunningApplication?
